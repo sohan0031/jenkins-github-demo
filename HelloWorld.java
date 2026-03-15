29758552cd4f08bb84818b06f93bb9c88621c2a5
sohan0031 <patilsohan0031@gmail.com>
2026-03-15 12:33:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Build!");
         System.out.println("Automatic Jenkins Build Trigger Test");
